4eb81ceb1eafa12744fbc1e958bf49f89106e26f
ferchoes16 <49039158+ferchoes16@users.noreply.github.com>
2019-04-24 00:09:58 -0500

    project properties

--- Project/SalesCompany/src/java/modelo/Operations.java
+++ Project/SalesCompany/src/java/modelo/Operations.java
@@ -7,7 +7,7 @@ package modelo;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 /**
@@ -15,33 +15,61 @@ import javax.swing.JOptionPane;
  * @author Mike
  */
 public class Operations {
-    
+
     private Conexion conex;
-    private PreparedStatement prepareStatement;
-    private ResultSet result;
-    private Statement statement;
+    private PreparedStatement pst;
+    private ResultSet rs;
     private String query;
-    
-    public Seller selectSeller(int idSeller){     
-        query = 
-                "select *\n"
-                + "from seller\n"
-                + "where idSeller = " + idSeller;
-        try{
+
+    public Seller selectSeller(int id) {
+        Seller seller = new Seller();
+        try {
             conex = new Conexion();
-            statement = conex.getConexion().createStatement();
-            result = statement.executeQuery(query);
-            if(result.next()){
-                return new Seller(result.getInt("idCourse"),result.getString("name"),result.getString("lastName"),result.getString("address"),result.getString("telephone"),result.getString("email"));
+            query = "Select * From seller where idSeller=" + id;
+            pst = conex.getConexion().prepareStatement(query);
+            rs = pst.executeQuery(query);
+            while (rs.next()) {
+                seller = new Seller(rs.getInt("idSeller"), rs.getString("name"), rs.getString("lastName"), rs.getString("address"), rs.getString("telephone"), rs.getString("email"));
             }
-            statement.close();
-            result.close();
-            conex.desconectar();
-        }catch(Exception e){
-            e.printStackTrace();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "No se pudo obtener datos");
+
         }
-        return null;
+        return seller;
+    }
+
+    public Ticket selectTicket(int id) {
+        Ticket ticket = new Ticket();
+        try {
+            conex = new Conexion();
+            query = "Select * From ticket where idTicket=" + id;
+            pst = conex.getConexion().prepareStatement(query);
+            rs = pst.executeQuery(query);
+            while (rs.next()) {
+                ticket = new Ticket(rs.getInt("idTicket"), rs.getInt("idSeller"), rs.getDouble("price"), rs.getString("departure"), rs.getString("arrival"), rs.getString("class"), rs.getDate("date"), rs.getTime("hour"), rs.getString("airline"), rs.getString("gate"), rs.getString("flightNumber"), rs.getString("seat"), rs.getInt("stock"), rs.getInt("discount"), rs.getBoolean("state"));
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "No se pudo obtener datos");
+
+        }
+        return ticket;
     }
     
-    
+    public Seller selectSellerTicket(int idTicket) {
+        Seller seller = new Seller();
+        try {
+            conex = new Conexion();
+            query = "SELECT * FROM seller where idSeller = (SELECT idSeller FROM ticket where idTicket ="+idTicket+")";
+            pst = conex.getConexion().prepareStatement(query);
+            rs = pst.executeQuery(query);
+            while (rs.next()) {
+                seller = new Seller(rs.getInt("idSeller"), rs.getString("name"), rs.getString("lastName"), rs.getString("address"), rs.getString("telephone"), rs.getString("email"));
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "No se pudo obtener datos");
+
+        }
+        return seller;
+    }
+
 }
